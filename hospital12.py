caf6d85cafa0aa568d614bc3938d4455c208fc99
aditya18-max <adityakatakanahalli@gmail.com>
2026-07-12 10:56:03 +0530

    Add files via upload

---
+++ hospital12.py
@@ -0,0 +1,18 @@
+patient_name="Aditya"
+age=4
+gender="Male"
+blood_group="A+"
+weight=68.00
+height=5.5
+consulation_fees=4500.00
+registration_charge=500.00
+total_amount_payable=consulation_fees+registration_charge
+print(f"patient name: {patient_name}")
+print(f"age: {age}")
+print(f"gender: {gender}")
+print(f"blood group: {blood_group}")
+print(f"weight: {weight}")
+print(f"height: {height}")
+print(f"consultation_fees: {consulation_fees}")
+print(f"registration fees: {registration_charge}")
+print(f"total amount payable: {total_amount_payable}")
